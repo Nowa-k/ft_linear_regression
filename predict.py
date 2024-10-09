9aac944e0112ccb8f252902ba0602f776f73747b
Alexis Ferrand <aleferra@z1r7p3.42lyon.fr>
2024-10-09 16:59:55 +0200

    Adapte code for noramalized value

--- predict.py
+++ predict.py
@@ -1,31 +1,29 @@
-import csv
 import numpy as np
+import pandas as pd
 
 class Predict:
     def __init__(self):
-        self.theta0 = 0.9019142003565301
-        self.theta1 = -0.90484545098481
+        self.theta0 = 0.90
+        self.theta1 = -0.90
         self.mileage = 0
-        self.x = []
-        self.y = 
+        self.mileage_normalized = 0
         
     def load_thetas(self):
-        value = []
-        with open("data.csv", mode='r') as file:
-            csv_reader = csv.reader(file)
-            header = next(csv_reader)
-            for row in csv_reader:
-                value.append(row)
-        self.theta0 = float(value[0][0])
-        self.theta1 = float(value[0][1]) 
+        df = pd.read_csv('data.csv')
+        self.x = df.iloc[:, 0].values
+        self.y = df.iloc[:, 1].values
 
     def estimate_price(self):
-        calc = self.theta0 + (self.theta1 * self.mileage)
-        calc = calc * (np.max(self.y) - np.min(self.y)) + np.min(self.y)
-        return self.theta0 + (self.theta1 * self.mileage)
+        # calc = self.theta0 + (self.theta1 * self.mileage)
+        # calc = calc * (np.max(self.y) - np.min(self.y)) + np.min(self.y)
+        predicted_price_normalized = self.theta0 + (self.theta1 * self.mileage_normalized)
+        predicted_price = predicted_price_normalized * (np.max(self.y) - np.min(self.y)) + np.min(self.y)
+        return predicted_price
+        # return self.theta0 + (self.theta1 * self.mileage)
 
     def input_km(self):
         self.mileage = float(input("Entrez le kilométrage de la voiture : "))
+        self.mileage_normalized = (self.mileage - np.min(self.x)) / (np.max(self.x) - np.min(self.x))
 
     def predict(self):
         estimate = self.estimate_price() 
@@ -33,6 +31,6 @@ class Predict:
 
 # Utilisation
 PRED = Predict()
+PRED.load_thetas()
 PRED.input_km()
-# PRED.load_thetas()
 PRED.predict()
