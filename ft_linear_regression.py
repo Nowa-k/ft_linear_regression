0438e58d0be2cb1cba1270d402f22934e635e89e
Alexis Ferrand <aleferra@z2r4p2.42lyon.fr>
2024-10-29 16:53:58 +0100

    end

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -1,5 +1,5 @@
+import csv
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 class LinearRegression:
@@ -9,6 +9,10 @@ class LinearRegression:
         self.y = []
         self.x_normalize = []
         self.y_normalize = []
+        self.x_mean = 0
+        self.x_std = 0
+        self.y_mean = 0
+        self.y_std = 0
         
     def read_csv(self):
         df = pd.read_csv('data.csv')
@@ -16,10 +20,22 @@ class LinearRegression:
         self.y = df.iloc[:, 1].values
     
     def normalize(self):
-        x = np.array(self.x)
-        self.x_normalize = (x - np.min(x)) / (np.max(x) - np.min(x))
-        y = np.array(self.y)
-        self.y_normalize = (y - np.min(y)) / (np.max(y) - np.min(y))
+        self.x_mean = self.ft_mean(self.x)
+        self.x_std = self.ft_std(self.x)
+        
+        self.y_mean = self.ft_mean(self.y)
+        self.y_std = self.ft_std(self.x)
+        
+        self.x_normalize = (self.x - self.x_mean) / self.x_std
+        self.y_normalize = (self.y - self.y_mean) / self.y_std
+        
+    def ft_std(self, values):
+            mean_val = self.ft_mean(values)
+            variance = sum((x - mean_val) ** 2 for x in values) / (len(values) - 1) if len(values) > 1 else 0
+            return variance ** 0.5
+        
+    def ft_mean(self, values):
+        return sum(values) / len(values)
         
     def calc_cost(self, theta_0, theta_1):
         global_cost = 0
@@ -48,35 +64,40 @@ class LinearRegression:
         theta_1 = tmp_theta1 - (learning_rate * derivate1)
         return [theta_0, theta_1]
     
-    def gradient_descent(self, iterations=500, learning_rate=0.1):
+    def gradient_descent(self, iterations=100, learning_rate=0.1):
         tmp_theta0 = 0.0
         tmp_theta1 = 0.0
         
         plt.ion()
-
         fig, ax = plt.subplots()
         ax.scatter(self.x, self.y, color="blue", label="Données")
         line, = ax.plot([], [], color="red", label="Ligne de régression")
-        plt.title("Régression linéaire : Km vs Prix")
+        plt.title("Régression linéaire : Prix d'une voiture en fonction de son kilomètrage")
         plt.xlabel("Kilométrage")
         plt.ylabel("Prix")
         plt.legend()
 
-        
         for i in range(iterations):
             tmp_theta0, tmp_theta1 = self.maj_theta(tmp_theta0, tmp_theta1, learning_rate)
-            
             regression_line = tmp_theta0 + tmp_theta1 * self.x_normalize
-            
-            line.set_data(self.x, regression_line * (np.max(self.y) - np.min(self.y)) + np.min(self.y))
-            
+            line.set_data(self.x, regression_line * self.y_std + self.y_mean)
             plt.draw()
             plt.pause(0.1)
-            
             cost = self.calc_cost(tmp_theta0, tmp_theta1)
             print(f"Iteration {i+1} | Cost: {cost} | Theta0: {tmp_theta0} | Theta1: {tmp_theta1}")
         
-        print(f"Final Theta0: {tmp_theta0}, Final Theta1: {tmp_theta1}")
+        theta1_denorm = tmp_theta1 * (self.y_std / self.x_std)
+        theta0_denorm = (tmp_theta0 * self.y_std) + self.y_mean - (theta1_denorm * self.x_mean)
+        
+        print(f"Final Theta0 (dénormalisé): {theta0_denorm}, Final Theta1 (dénormalisé): {theta1_denorm}")
+        data = [
+            ['theta0', 'theta1'],
+            [theta0_denorm, theta1_denorm]
+        ]
+        with open('theta.csv', mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+        
         plt.ioff()
         plt.show()
 
--- predict.py
+++ predict.py
@@ -1,35 +1,37 @@
-import numpy as np
 import pandas as pd
 
 class Predict:
     def __init__(self):
-        self.theta0 = 0.90
-        self.theta1 = -0.90
+        self.theta0 = 0
+        self.theta1 = 0
         self.mileage = 0
-        self.mileage_normalized = 0
         
     def load_thetas(self):
-        df = pd.read_csv('data.csv')
-        self.x = df.iloc[:, 0].values
-        self.y = df.iloc[:, 1].values
+        df = pd.read_csv('theta.csv')
+        thetas = df.iloc[0]
+        self.theta0 = thetas.iloc[0]
+        self.theta1 = thetas.iloc[1]
 
     def estimate_price(self):
-        # calc = self.theta0 + (self.theta1 * self.mileage)
-        # calc = calc * (np.max(self.y) - np.min(self.y)) + np.min(self.y)
-        predicted_price_normalized = self.theta0 + (self.theta1 * self.mileage_normalized)
-        predicted_price = predicted_price_normalized * (np.max(self.y) - np.min(self.y)) + np.min(self.y)
-        return predicted_price
-        # return self.theta0 + (self.theta1 * self.mileage)
+        return (self.theta0 + (self.theta1 * self.mileage))
 
     def input_km(self):
-        self.mileage = float(input("Entrez le kilométrage de la voiture : "))
-        self.mileage_normalized = (self.mileage - np.min(self.x)) / (np.max(self.x) - np.min(self.x))
-
+        try :
+            self.mileage = int(input("Entrez le kilomètrage de la voiture : "))
+        except ValueError:
+            print("Mettez un entier")
+            exit()
+        if self.mileage < 0: 
+            print("Mettez un nombre de kilomètre positif")
+            exit()
+        if self.mileage > 375000:
+            print("Mettez un nombre de kilomètrage inférieur à 500 000")
+            exit()
+ 
     def predict(self):
         estimate = self.estimate_price() 
         print(f"Le prix estimé de la voiture pour {self.mileage} km est : {estimate:.2f} euros.")
 
-# Utilisation
 PRED = Predict()
 PRED.load_thetas()
 PRED.input_km()
